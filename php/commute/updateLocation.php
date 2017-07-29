a4966808faaadeaa8e879f6d42b31f16819e8881
ShubhiL <shubhi.lohani@gmail.com>
2017-07-29 12:21:22 +0530

    Update updateLocation.php

--- php/commute/updateLocation.php
+++ php/commute/updateLocation.php
@@ -1,4 +1,4 @@
-<?php // 
+<?php
  
    require_once 'include/DB_Functions.php';
     $db = new DB_Functions();
@@ -8,28 +8,26 @@
 	 
 	 if (isset($_POST['driver_id'])&& isset($_POST['current_lat'])&& isset($_POST['current_long']))
 	 {
-	    $driver_id = $_POST['driver_id'];
+	    	$driver_id = $_POST['driver_id'];
 		$current_lat = $_POST['current_lat'];
 		$current_long = $_POST['current_long'];
 		
-		
 		if ($db->updateLatLong($driver_id, $current_lat, $current_long))
 		{
-		
-        $response["response"] = "Location updated";
-        echo json_encode($response);
+			$response["response"] = "Location updated";
+			echo json_encode($response);
 		}
 		else {
-        // user is not found with the credentials
-        $response["error"] = TRUE;
-        $response["error_msg"] = "One or more parameters is not correct. Please try again!";
-        echo json_encode($response);
-    }
+        	// user is not found with the given credentials
+			$response["error"] = TRUE;
+			$response["error_msg"] = "One or more parameters is not correct. Please try again!";
+			echo json_encode($response);
+  	  }
 	}
-		else {
-    //required post params is missing
-    $response["error"] = TRUE;
-    $response["error_msg"] = "Required parameters are missing!";
-    echo json_encode($response);
+	else {
+    //required  params is missing
+	    $response["error"] = TRUE;
+	    $response["error_msg"] = "Required parameters are missing!";
+	    echo json_encode($response);
             }
-			?>
+?>
