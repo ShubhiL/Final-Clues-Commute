e5a2db008c7d0823609e6029f67bfb84928af607
ShubhiL <shubhi.lohani@gmail.com>
2017-07-29 12:23:28 +0530

    Update sameZoneDrivers.php

--- php/commute/sameZoneDrivers.php
+++ php/commute/sameZoneDrivers.php
@@ -8,7 +8,7 @@ $response = array("error" => FALSE);
 // json response array 
 if (isset($_GET['employee_id'])) {
  
-    // receiving the post params
+    // receiving the params
     $employee_id= $_GET['employee_id'];
 
     if($db->fetchFromUsersAccessType($employee_id)==="passenger"){
@@ -17,31 +17,28 @@ if (isset($_GET['employee_id'])) {
     if($result){
         for ($i=0; $i<count($result); $i++)
         {
-            //employee_id, employee_name, address, designation, owns_vehicle, mobile_number, gender 
+            //fetching employee_id, employee_name, address, designation, owns_vehicle, mobile_number, gender 
 
-            $response["response"][$i] = array("employee_id"=>$result[$i][0],"employee_name"=>$result[$i][1],"address"=>$result[$i][2],"designation"=>$result[$i][3],"owns_vehicle"=>$result[$i][4],"mobile_number"=>$result[$i][5],"gender"=>$result[$i][6]);
-            
-
+            $response["response"][$i] = array("employee_id"=>$result[$i][0],"employee_name"=>$result[$i][1],"address"=>$result[$i][2],"designation"=>$result[$i][3],"owns_vehicle"=>$result[$i][4],"mobile_number"=>$result[$i][5],"gender"=>$result[$i][6]); 
         }
     echo json_encode($response);
     }
     else{
-        $response["error"] = TRUE;
+            $response["error"] = TRUE;
             $response["error_msg"] = "Sorry! No Drivers in Your Zone";
             echo json_encode($response);
-    }  
+        }  
     }
     else{
-        $response["error"] = TRUE;
+            $response["error"] = TRUE;
             $response["error_msg"] = "Not a Passenger";
             echo json_encode($response);
-    }
-    
+        }
     }
 
     else{
         $response["error"] = TRUE;
-    $response["error_msg"] = "Required parameter (employee_id) is missing!";
-    echo json_encode($response);
+        $response["error_msg"] = "Required parameter (employee_id) is missing!";
+        echo json_encode($response);
     }
 ?>
