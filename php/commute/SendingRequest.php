a9517f8e363c2295fd72612b58c2e1f124abddc5
ShubhiL <shubhi.lohani@gmail.com>
2017-07-29 12:14:33 +0530

    Update SendingRequest.php

--- php/commute/SendingRequest.php
+++ php/commute/SendingRequest.php
@@ -1,33 +1,31 @@
  
  <?php  // API TO SEND REQUEST
        
-	   require_once 'include/DB_Functions.php';
-       $db = new DB_Functions(); 
+	require_once 'include/DB_Functions.php';
+       	$db = new DB_Functions(); 
 
-	   // json response array
-	   
+	// json response array  
       $response = array("error" => FALSE);
       									 
       if (isset($_GET['requester_id']) && isset($_GET['requestee_id']))
       {
-       
-          // receiving the post parameters
-          
+          // receiving the parameters
           $requester_id = $_GET['requester_id'];
           $requestee_id = $_GET['requestee_id'];
           
           //checking access type
           if($db->fetchFromUsersAccessType($requester_id)==="driver"){
-
-            // IF  request is delivered to server
-            if ($db->doRequestExisted($requester_id,$requestee_id)){ // calling function
+	
+	//FOR ACCESS_TYPE = DRIVER	  
+            // IF  request already exist in REQUEST table
+            if ($db->doRequestExisted($requester_id,$requestee_id)){ 
 
                   $response["error"]= TRUE;
-                  $response["error_msg"] = "Request has been already exist in Request table";
+                  $response["error_msg"] = "Request already exist in Request table";
                   echo json_encode($response);
               }
-              // store request in database .... -> object operator... call method store requeston $db object
-              else if ($db->storeRequest($requester_id, $requestee_id, $requester_id))  // calling function
+              // store request in database
+              else if ($db->storeRequest($requester_id, $requestee_id, $requester_id))
                  {
                     $response['error'] = FALSE;
                     $response['response'] = TRUE;
@@ -35,7 +33,6 @@
               }
               
               else {
-
                   $response["error"] = TRUE;
                   $response['error_msg'] = "Request Cannot be Sent";
                   echo json_encode($response);
@@ -44,16 +41,15 @@
           }
 
           else{
-
-            // IF  request is delivered to server
-            if ($db->doRequestExisted($requestee_id,$requester_id)){ // calling function
+		  //FOR ACCESS TYPE = PASSENGER
+            if ($db->doRequestExisted($requestee_id,$requester_id)){ 
 
                   $response["error"]= TRUE;
                   $response["error_msg"] = "Request has been already sent";
                   echo json_encode($response);
               }
-              // store request in database .... -> object operator... call method store requeston $db object
-              else if ($db->storeRequest($requestee_id, $requester_id, $requester_id))  // calling function
+              
+              else if ($db->storeRequest($requestee_id, $requester_id, $requester_id))
                  {
                     $response['error'] = FALSE;
                     $response['response'] = TRUE;
